2505a3b477fbdd83b631428b65a56e5f12aff635
ThaiBao011002 <123188501+ThaiBao011002@users.noreply.github.com>
2023-03-02 02:08:54 +0700

    Code moi

--- Js/Main.js
+++ Js/Main.js
@@ -3,6 +3,7 @@
 var NumArray = [];
 var ArrayNumberPositive = [];
 var NumberEven = [];
+
 function Addnumber() {
     var Num = Number(document.getElementById("Number").value);
     NumArray.push(Num);
@@ -87,7 +88,7 @@ document.getElementById("ChangePlaces").onclick = function Change (){
     value2 = NumArray[Places2];
 
     [value1, value2] = [value2, value1];
-    document.getElementById("LastChange").innerHTML = value1, value2;
+    // document.getElementById("LastChange").innerHTML = NumArray;
 }
 
 // Sắp xếp số
@@ -108,5 +109,73 @@ function SortNumber() {
     document.getElementById("Arrange_Btn").onclick = SortNumber;
 
 // Số nguyên tố đầu tiên
+function isprime(n){
+    let flag = 1;
+    if (n <2) return flag = 0;    
+    let i = 2;
+    while(i < n ){
+        if( n % i == 0 ) {
+            flag = 0;
+            break;
+        }
+        i++;
+    }
+
+    return flag;
+}
+document.getElementById("FindNumberPrime").onclick = function FindNumberPrime (){
+    var NumberPrime = [];
+    var Num = NumArray[0];
+
+    for (let i = 0; i < NumArray.length; i++){
+        if (isprime(NumArray[i]) == 1)  
+        NumberPrime.push( NumArray[i] );
+        Num = NumberPrime[0];
+      }
+    document.getElementById("FirstNumberPrime").innerHTML = Num;
+}
 
 
+// Đếm số nguyên
+function AddnumberInt() {
+    var Num = Number(document.getElementById("NumberInt").value);
+    NumArray.push(Num);
+  
+    
+document.getElementById("ArrayInt").innerHTML = NumArray;
+}
+document.getElementById("addnumberint").onclick = AddnumberInt;
+
+document.getElementById("CountNumberIntBtn").onclick = function Count (){
+    var count = 0;
+    for (var i = 0; i < NumArray.length; i++) {
+        if (Number.isInteger(NumArray[i]) == true){
+            count++;
+        }  }  
+    document.getElementById("CountNumberInt").innerHTML = count; 
+
+}
+
+// So sánh số dương và âm
+document.getElementById("Compare").onclick = function CompareNumber (){
+    var number1 = [];
+    var number2 = [];
+     for (var i = 0; i < NumArray.length; i++) {
+        if (NumArray[i] > 0){
+            number1.push(NumArray[i]);
+            console.log(number1);  
+        } else {
+            number2.push(NumArray[i]);
+            console.log(number2);
+        }
+       
+     }
+     if (number1.length > number2.length){
+        document.getElementById("ComparisonResults").innerHTML = "Số dương > Số âm";
+     } else if (number1.length < number2.length){
+        document.getElementById("ComparisonResults").innerHTML = "Số âm > Số dương";
+     } else {
+        document.getElementById("ComparisonResults").innerHTML = "Số dương = Số âm";
+
+     }
+}
